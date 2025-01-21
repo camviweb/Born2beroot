d41506a5e59f58f55e417e1ca4bc84c1e301c6b0
Câm-Vi Nguyen <167216844+camviweb@users.noreply.github.com>
2025-01-21 13:39:26 +0100

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
-
-archi=$(uname -a)
+archi=$(uname -srvmo)
 cpup=$(grep "physical id" /proc/cpuinfo | wc -l) 
 cpuv=$(nproc)
-ram_total=$(free -m | awk '$1 == "Mem:" {print $2}')
+ram_tot=$(free -m | awk '$1 == "Mem:" {print $2}')
 ram_use=$(free -m | awk '$1 == "Mem:" {print $3}')
-ram_percent=$(awk "BEGIN {printf("%.2f", ${ram_use}/${ram_total}*100)}")
-disk_total=$(df -BG | grep '^/dev/' | grep -v '/boot$' | awk '{ft += $2} END {print ft}')
-disk_use=$(df -BM | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} END {print ut}')
-disk_percent=$(awk "BEGIN {print int($disk_use/$disk_total*100)}")
-cpul=$(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}')
-lb=$(who -b | awk '$1 == "system" {print $3 " " $4}')
-lvmu=$(if [ $(lsblk | awk '{print $6}' | grep "lvm" | wc -l) -eq 0 ]; then echo no; else echo yes; fi)
+ram_per=$(awk "BEGIN {printf \"%.2f\", ($ram_use/$ram_tot)*100}")
+disk_tot=$(df -BG --total | grep 'total' | awk '{print $2}')
+disk_use=$(df -BG --total | grep 'total' | awk '{print $3}')
+disk_per=$(df -BG --total | grep 'total' | awk '{print $5}')
+cpu_per=$(echo $[100-$(vmstat 1 2|tail -1|awk '{print $15}')])
+lb=$(who -b | awk '{print($3 " " $4)}')
+lvmu=$(if [ $(lsblk | grep "lvm" | wc -l) -eq 0 ]; then echo 'Non'; else echo 'Oui'; fi)
 conn=$(ss -Ht state established | wc -l)
 ulog=$(users | wc -w)
 ip=$(hostname -I)
 mac=$(ip link show | grep "ether" | awk '{print $2}')
-sudo=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
+sudo=$(journalctl _COMM=sudo | grep "COMMAND" | wc -l)
 wall "  #Architecture: $archi
         #CPU physical: $cpup
         #vCPU: $cpuv
-        #Memory Usage: $ram_use/${ram_total}MB ($ram_percent%)
-        #Disk Usage: $udisk/${fdisk}Gb ($pdisk%)
-        #CPU load: $cpul
+        #Memory Usage: $ram_use/${ram_tot}MB ($ram_per%)
+        #Disk Usage: $disk_use/${disk_tot} ($disk_per)
+        #CPU load: $cpu_per%
         #Last boot: $lb
         #LVM use: $lvmu
         #Connections TCP: $conn ESTABLISHED
